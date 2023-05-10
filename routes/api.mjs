b6e528a86108169663f82c81c78ce3379b70e3a7
Walter Pérez <jose.perezt@salud.gob.sv>
2023-05-10 17:06:36 -0600

    POST Y GET de puesto trabajo dependencia terminado

--- routes/api.mjs
+++ routes/api.mjs
@@ -3,11 +3,12 @@ import authRouter from './api/auth.mjs'
 import puestoTrabajoRouter from './api/puestoTrabajo.mjs'
 import tipoDependenciaRouter  from './api/tipoDependencia.mjs'
 import dependenciaRouter from './api/dependencia.mjs'
+import puestoTrabajoDependenciaRouter from './api/puestoTrabajoDependencia.mjs'
 import {validateToken} from "../app/middlewares/AuthMiddlewares.mjs";
 const router = Router();
 
 router.use('/auth',authRouter)
 router.use('/puesto_trabajo',[validateToken],puestoTrabajoRouter)
 router.use('/tipo_dependencia',[validateToken],tipoDependenciaRouter)
-router.use('/dependencia',[validateToken], dependenciaRouter)
+router.use('/puesto_dependencia',[validateToken], puestoTrabajoDependenciaRouter)
 export default router;
