b801b494be6629edcb3fd95fb0dd305d74459b8f
WalterPrz <jose.perezt@salud.gob.sv>
2023-06-07 15:18:28 -0600

    validacion de planilla

---
+++ app/Schemas/Planilla/updateSchema.mjs
@@ -0,0 +1,18 @@
+import { Planilla } from "../../models/index.mjs";
+import Sequelize, { Op } from "sequelize";
+import { verifyDataExist, callValidateFunc } from '../utils.mjs'
+import moment from "moment";
+const customVerifyExist = callValidateFunc(verifyDataExist);
+const createCentroCostoSchema = {
+    id_planilla: {
+        isInt: {
+            bail: true,
+            errorMessage: "Valor incorrecto en id de la planilla",
+        },
+        custom: {
+            bail: true,
+            options: customVerifyExist('id_planilla', Planilla)
+        },
+    }
+};
+export default createCentroCostoSchema;
